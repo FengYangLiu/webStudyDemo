8ad9780305912871eb3223d346546e4689e9e3c0
coderlfy <846287593@qq.com>
2018-07-20 14:26:39 +0800

    project init
    项目初始化

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -3,13 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/style/reset.css'
+import './assets/style/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
