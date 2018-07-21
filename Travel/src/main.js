d6794cd8b30a02d6dc5582463ba7c0af57288163
coderlfy <846287593@qq.com>
2018-07-21 21:29:55 +0800

    index-swiper

--- Travel/src/main.js
+++ Travel/src/main.js
@@ -1,12 +1,19 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+
+// ==== s vue 轮播 ====
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+Vue.use(VueAwesomeSwiper)
+// ==== e vue 轮播 ====
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
---
+++ Travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="wrapper">
+  <swiper :options="swiperOption">
+  <swiper-slide v-for="item of swiperList" :key="item.id">
+  <img class="swiper-img" :src="item.imgUrl" alt="" />
+  </swiper-slide>
+  <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '001',
+        imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'
+      },
+      {
+        id: '002',
+        imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20180713_CPM_11053.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active {
+  background: #fff;
+}
+
+.wrapper {
+  width: 100%;
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 24.53%;
+
+  .swiper-img {
+    width: 100%;
+  }
+}
+</style>
