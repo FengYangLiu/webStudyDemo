68483b85b26b27fe4cae645abf9f41974294d6be
coderlfy <846287593@qq.com>
2018-07-22 19:17:54 +0800

    add recommend

--- Travel/src/pages/home/components/Icons.vue
+++ Travel/src/pages/home/components/Icons.vue
@@ -85,32 +85,34 @@ export default {
   overflow hidden
   height 0
   padding-bottom 50%
-.icon
-  position relative
-  width 25%
-  float left
-  padding-bottom 25%
-  overflow hidden
-  .icon-img
-    position absolute
-    top 0
-    left 0
-    right 0
-    bottom .44rem
+.icons
+  margin-top .1rem
+  .icon
+    position relative
+    width 25%
+    float left
+    padding-bottom 25%
+    overflow hidden
+    .icon-img
+      position absolute
+      top 0
+      left 0
+      right 0
+      bottom .44rem
 
-    .icon-img-content
-      display block
-      margin 0 auto
-      height 100%
-  .icon-desc
-    position absolute
-    left 0
-    right 0
-    bottom 0
-    height .44rem
-    line-height .44rem
-    color $darkTextColor
-    text-align center
-    ellipsis()
+      .icon-img-content
+        display block
+        margin 0 auto
+        height 100%
+    .icon-desc
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      height .44rem
+      line-height .44rem
+      color $darkTextColor
+      text-align center
+      ellipsis()
 
 </style>
---
+++ Travel/src/pages/home/components/Recommend.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+     <div class="title">热销推荐</div>
+     <ul>
+       <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgUrl" alt="item.title">
+         <div class="item-info">
+           <p class="item-title">{{item.title}}</p>
+           <p class="item-desc">{{item.desc}}</p>
+           <button class="item-button">查看详情</button>
+         </div>
+       </li>
+     </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201408/14/dba7220ac844600c07a1af703f640935.jpg_200x200_a84b7d79.jpg',
+        title: '杭州乐园',
+        desc: '杭州乐园杭州乐园杭州乐园杭州乐园杭州乐园杭州乐园杭州乐园'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201408/14/dba7220ac844600c07a1af703f640935.jpg_200x200_a84b7d79.jpg',
+        title: '杭州乐园',
+        desc: '杭州乐园杭州乐园杭州乐园杭州乐园杭州乐园杭州乐园杭州乐园'
+      }, {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201408/14/dba7220ac844600c07a1af703f640935.jpg_200x200_a84b7d79.jpg',
+        title: '杭州乐园',
+        desc: '杭州乐园杭州乐园杭州乐园杭州乐园杭州乐园杭州乐园杭州乐园'
+      }, {
+        id: '004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201408/14/dba7220ac844600c07a1af703f640935.jpg_200x200_a84b7d79.jpg',
+        title: '杭州乐园',
+        desc: '杭州乐园杭州乐园杭州乐园杭州乐园杭州乐园杭州乐园杭州乐园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/mixins.styl'
+.title
+  margin-top .2rem
+  line-height .8rem
+  background #eeeeee
+  text-indent .2rem
+.item
+  display flex
+  height 1.9rem
+  overflow hidden
+  .item-img
+    width 1.7rem
+    height 1.7rem
+    padding .1rem
+  .item-info
+    min-width 0
+    flex 1
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      color #ccc
+      ellipsis()
+    .item-button
+      line-height .44rem
+      background #ff9300
+      margin-top .16rem
+      padding 0 .2rem
+      border-radius .06rem
+      color #fff
+</style>
