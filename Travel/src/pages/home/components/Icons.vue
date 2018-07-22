f7320f914a896e6df8e3a8cef6d8bfb1d3fc3573
coderlfy <846287593@qq.com>
2018-07-22 16:45:48 +0800

    add icons

---
+++ Travel/src/pages/home/components/Icons.vue
@@ -0,0 +1,116 @@
+<template>
+  <div class="icons">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+      <div class="icon" v-for="item of page" :key="item.id">
+        <div class="icon-img">
+         <img class="icon-img-content" :src="item.imgUrl" alt="">
+        </div>
+        <p class="icon-desc">{{item.desc}}</p>
+      </div>
+    </swiper-slide>
+
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      swiperOption: {},
+      iconList: [{
+        id: '001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      }, {
+        id: '003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+        desc: '主题公园'
+      }, {
+        id: '004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/75/eca3ce656c886502.png',
+        desc: '漂流'
+      }, {
+        id: '005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+        desc: '景点门票'
+      }, {
+        id: '006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+        desc: '一日游'
+      }, {
+        id: '007',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
+        desc: '主题公园'
+      }, {
+        id: '008',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/75/eca3ce656c886502.png',
+        desc: '漂流'
+      }, {
+        id: '009',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/80/416c6ab3368d1f02.png',
+        desc: '全部玩乐'
+      }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.iconList.forEach((item, i) => {
+        const page = Math.floor(i / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+@import '~styles/mixins.styl'
+
+.icons >>> .swiper-container
+  width 100%
+  overflow hidden
+  height 0
+  padding-bottom 50%
+.icon
+  position relative
+  width 25%
+  float left
+  padding-bottom 25%
+  overflow hidden
+  .icon-img
+    position absolute
+    top 0
+    left 0
+    right 0
+    bottom .44rem
+
+    .icon-img-content
+      display block
+      margin 0 auto
+      height 100%
+  .icon-desc
+    position absolute
+    left 0
+    right 0
+    bottom 0
+    height .44rem
+    line-height .44rem
+    color $darkTextColor
+    text-align center
+    ellipsis()
+
+</style>
