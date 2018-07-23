b5bcd24fbba68a4da4b00720f6109b4fd9102ea7
coderlfy <846287593@qq.com>
2018-07-23 20:22:30 +0800

    city router

--- Travel/src/pages/home/components/Header.vue
+++ Travel/src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
       <span class="iconfont">&#xe632;</span>
       输入城市/景点/游玩主题
     </div>
-    <div class="header-right">
-      {{this.city}}
-      <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+      <div class="header-right">
+        {{this.city}}
+        <span class="iconfont arrow-icon">&#xe64a;</span>
+      </div>
+    </router-link>
   </div>
 </template>
 <script>
@@ -24,24 +26,20 @@ export default {
 
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl';
-
 .header {
   display: flex;
-  height: 0.86rem;
-  line-height: 0.86rem;
+  height: $headerHeight
+  line-height: $headerHeight
   background: $bgColor;
   color: #ffffff;
-
   .header-left {
     width: 0.64rem;
     float: left;
-
     .back-icon {
       text-align: center;
       font-size: 0.4rem;
     }
   }
-
   .header-input {
     flex: 1;
     height: 0.64rem;
@@ -53,12 +51,11 @@ export default {
     border-radius: 0.1rem;
     color: #ccc;
   }
-
   .header-right {
     width: 1.24rem;
     float: right;
     text-align: center;
-
+    color #fff
     .arrow-icon {
       margin-left: -0.04rem;
       font-size: 0.24rem;
