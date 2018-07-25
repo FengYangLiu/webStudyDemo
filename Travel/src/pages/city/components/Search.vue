3bae43722514e28c138d2ebb2ec41fc21b6ca4fb
coderlfy <846287593@qq.com>
2018-07-25 18:30:37 +0800

    search finish

--- Travel/src/pages/city/components/Search.vue
+++ Travel/src/pages/city/components/Search.vue
@@ -1,27 +1,93 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或者拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或者拼音">
+    </div>
+    <div class="search-content" v-show="keyword" ref="search">
+      <ul>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (const i in this.cities) {
+          if (this.cities.hasOwnProperty(i)) {
+            const element = this.cities[i]
+            element.forEach((value) => {
+              if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+                result.push(value)
+              }
+            })
+          }
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
+  }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '~styles/varibles.styl'
 .search
-  height .72rem
-  padding 0 .1rem
-  background $bgColor
+  height: 0.72rem
+  padding: 0 0.1rem
+  background: $bgColor
   .search-input
-    box-sizing border-box
-    width 100%
-    height .62rem
-    padding 0 .1rem
+    box-sizing: border-box
+    width: 100%
+    height: 0.62rem
+    padding: 0 0.1rem
+    line-height: 0.62rem
+    text-align: center
+    border-radius: 0.06rem
+.search-content
+  position: absolute
+  top: 1.58rem
+  left: 0
+  right: 0
+  bottom: 0
+  z-index: 1
+  overflow: hidden
+  background: #fff
+  .search-item
     line-height .62rem
-    text-align center
-    border-radius .06rem
+    padding-left .2rem
+    background #ffffff
+    color #666
 </style>
