370f51be41749ceca8f7e0bab494bc89d242e3f3
coderlfy <846287593@qq.com>
2018-07-25 15:29:20 +0800

    perfomance

--- Travel/src/pages/city/components/Alphabet.vue
+++ Travel/src/pages/city/components/Alphabet.vue
@@ -1,6 +1,12 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
+    <li class="item" v-for="item of letters"
+    :key="item"
+    :ref="item"
+    @touchstart="handleTouchStart"
+    @touchmove="handleTouchMove"
+    @touchend="handleTouchEnd"
+    @click="handleLetterClick">{{item}}</li>
   </ul>
 </template>
 
@@ -9,24 +15,73 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (const i in this.cities) {
+        if (this.cities.hasOwnProperty(i)) {
+          letters.push(i)
+        }
+      }
+      return letters
+    }
+  },
+  methods: {
+    handleLetterClick (evt) {
+      this.$emit('change', evt.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (evt) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          const touchY = evt.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index <= this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }, 16)
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-@import '~styles/varibles.styl'
-.list
-  display flex
-  flex-direction column
-  justify-content center
-  position absolute
-  top 1.58rem
-  right 0
-  bottom 0
-  width .4rem
-  .item
-    line-height .4rem
-    text-align center
-    color $bgColor
+@import '~styles/varibles.styl';
 
+.list {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: absolute;
+  top: 1.58rem;
+  right: 0;
+  bottom: 0;
+  width: 0.4rem;
+
+  .item {
+    line-height: 0.4rem;
+    text-align: center;
+    color: $bgColor;
+  }
+}
 </style>
